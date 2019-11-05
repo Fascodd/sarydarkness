0c3a3a979d061b2beca65a3c7dbb1572f0c3ba40
Fascodd <45606128+Fascodd@users.noreply.github.com>
2019-11-05 09:35:11 -0800

    Remake
    Removing most of the code in order to start over

--- sarydarkness/src/App.js
+++ sarydarkness/src/App.js
@@ -21,47 +21,11 @@ class App extends React.Component {
 
   }
   Fade = () => {
-    let enterContainer = document.getElementById("enter-container")
-    let mainBody = document.getElementById("main-body");
-    enterContainer.classList.add('inactive');
-    mainBody.classList.add('active')
-
-    setTimeout(() => { enterContainer.style.display = "none" }, 500)
   }
 
   render() {
     return (
       <div className="App">
-        <div className="container-area">
-          <div className='video-container'>
-            <video className='bg-video' loop muted autoPlay >
-              <source src={this.state.video3} type="video/webm" />
-              <source src={this.state.video3} type="video/mp4" />
-              <source src={this.state.video3} type="video/ogg" />
-            </video>
-            <div className="fullscreen-video-wrap"></div>
-          </div>
-          <div id="enter-container" >
-            <button className="enter-button " onClick={this.Fade}>Enter</button>
-          </div>
-          <div id="main-body">
-            <header className="header-area">
-              <ul className="header-list">
-                {this.state.itemList.map(item => {
-                  return <li className="header-item list-item" key={item.key}>{`${item.text}`}</li>
-                })}
-              </ul>
-            </header>
-            <div className="left-navbar">
-              <ul className="left-navlist">
-                <li className="list-item">Catagory1</li>
-                <li className="list-item">Catagory2</li>
-                <li className="list-item">Catagory3</li>
-              </ul>
-            </div>
-          </div>
-
-        </div>
       </div>
     );
   }
