e81616a40cadcbf3f6871c950fcfdcb36c53b942
Fascodd <45606128+Fascodd@users.noreply.github.com>
2019-11-06 12:14:00 -0800

    showcase and cataogry selector setup
    setup showcase with grayscale filter as well as catagory selector onClick handler to change style

--- sarydarkness/src/App.js
+++ sarydarkness/src/App.js
@@ -2,28 +2,55 @@ import React from 'react';
 import './App.css';
 import joker_pic from './pics/joker_2019.png'
 import pink_milk from './pics/pink_milk.PNG'
+import neon_demon from './pics/neon_demon.PNG'
 import video3 from './videos/Pink Milk _ Speed Paint.mp4'
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      jokerPic:joker_pic,
-      pinkMilk:pink_milk,
+      jokerPic: joker_pic,
+      pinkMilk: pink_milk,
+      neon_demon,
       video3: video3,
       itemList: [
-        { key: 1, text: "One" },
-        { key: 2, text: "Two" },
-        { key: 3, text: "Three" }],
-      navList: ["Catagory 1", "Catagory 2", "Catagory 3"]
-
+        { id: 1, text: "One" },
+        { id: 2, text: "Two" },
+        { id: 3, text: "Three" }],
+      navList: ["Catagory 1", "Catagory 2", "Catagory 3"],
+      catList: [
+        { id: 1, name: "Catagory1", classes: "catagory filter-item active-cat", selected: true },
+        { id: 2, name: "Catagory2", classes: "catagory filter-item", selected: false },
+        { id: 3, name: "Catagory3", classes: "catagory filter-item", selected: false },
+        { id: 4, name: "Catagory4", classes: "catagory filter-item", selected: false }]
     }
-    this.Fade = this.Fade.bind(this);
 
+    this.Fade = this.Fade.bind(this);
+    this.OnCatagoryClick = this.OnCatagoryClick.bind(this);
   }
   Fade = () => {
   }
+  OnCatagoryClick = (e) => {
+    // Changes selected catagory style
+    let CatagoryItemChange = () => {
+      if (e.target.tagName == "LI") {
+        let selectedCat = this.state.catList.filter(obj => e.target.id == obj.id)
+        this.setState(prevstate => ({
+          catList: prevstate.catList.map(
+            obj =>
+              obj.id == selectedCat[0].id ? { ...obj, classes: "catagory filter-item active-cat", selected: true } :
+                { ...obj, classes: "catagory filter-item", selected: false }
+          )
+        }))
+      }
+    };
 
+    CatagoryItemChange();
+  }
   render() {
+    // list of filterable gallery catagories
+    let catagories = this.state.catList.map(obj => <li key={obj.id} id={obj.id} className={obj.classes}>{obj.name}</li>
+    )
+
     return (
       <div className="App">
         <div id="container">
@@ -39,9 +66,17 @@ class App extends React.Component {
           </header>
 
           <div className="showcase">
-            <div style={{backgroundImage:`url(${this.state.jokerPic})`}} ></div>
-            <div></div>
-            <div></div>
+            <div className="showcase-img" style={{ backgroundImage: `url(${this.state.jokerPic})` }} ></div>
+            <div className="showcase-img" style={{ backgroundImage: `url(${this.state.pinkMilk})` }} ></div>
+            <div className="showcase-img" style={{ backgroundImage: `url(${this.state.neon_demon})` }} ></div>
+            <div className="showcase-text-wrapper">
+              <h1 className="showcase-header">Lorem ipsum, dolor sit amet consectetur</h1>
+              <p className="showcase-text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Ipsam nemo minus doloribus quod enim totam eligendi laudantium
+                 quibusdam ea sapiente nulla quisquam aliquam, inventore accusantium
+               odio quos accusamus hic! Molestias!</p>
+            </div>
           </div>
           <div className="filter-selector">
             <div className="dropdown">
@@ -49,16 +84,13 @@ class App extends React.Component {
               <div className="dropdown-content">
               </div>
             </div>
-            <ul>
-              <li className="catagory filter-item">Catagory1</li>
-              <li className="catagory filter-item">Catagory2</li>
-              <li className="catagory filter-item">Catagory3</li>
-              <li className="catagory filter-item">Catagory4</li>
+            <ul onClick={this.OnCatagoryClick}>
+              {catagories}
             </ul>
 
           </div>
           <div className="gallery">
-            <img src="#"></img>
+
           </div>
 
         </div>
---
+++ sarydarkness/src/pics/images.js
@@ -0,0 +1,14 @@
+const images = [
+    { id: 1, src:'showcase_img/joker_2019.png' },
+    { id: 2, src: './neon_demon.PNG' },
+    { id: 3, src: './pink_milk.PNG' },
+]
+export default images;
+//
+//{id:4,src:'./gallery_img/joker_1.png'},
+//{id:5,src:'./gallery_img/joker_2.png'},
+//{id:6,src:'./gallery_img/study_1.png'},
+//{id:7,src:'./gallery_img/study_2.png'},
+//{id:8,src:'./gallery_img/study_3.png'},
+//{id:9,src:'./gallery_img/study_4.png'},
+//{id:10,src:'./gallery_img/study_5.png'},
