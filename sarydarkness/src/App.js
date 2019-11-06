cd2e0f808963a0d0b7fa523dd7aaa602ed2c2502
Fascodd <45606128+Fascodd@users.noreply.github.com>
2019-11-05 17:36:40 -0800

    Created of new site
    Setup the divs  and lists to get a feel for the layout. Colored baclgrounds to make sure that I know where things are.

--- sarydarkness/src/App.js
+++ sarydarkness/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import video1 from './videos/Cyborg _ Speed Paint.mp4'
-import video2 from './videos/Neon Demon.mp4'
+import joker_pic from './pics/joker_2019.png'
+import pink_milk from './pics/pink_milk.PNG'
 import video3 from './videos/Pink Milk _ Speed Paint.mp4'
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      video1: video1,
-      video2: video2,
+      jokerPic:joker_pic,
+      pinkMilk:pink_milk,
       video3: video3,
       itemList: [
         { key: 1, text: "One" },
@@ -26,6 +26,42 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <div id="container">
+          <header>
+            <div id="signature">
+              <img src="#"></img>
+            </div>
+            <ul>
+              <li className="catagory header-nav-item">Home </li>
+              <li className="catagory header-nav-item">Info</li>
+              <li className="catagory header-nav-item">Request</li>
+            </ul>
+          </header>
+
+          <div className="showcase">
+            <div style={{backgroundImage:`url(${this.state.jokerPic})`}} ></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="filter-selector">
+            <div className="dropdown">
+              <button className="dropbtn">Dropdown</button>
+              <div className="dropdown-content">
+              </div>
+            </div>
+            <ul>
+              <li className="catagory filter-item">Catagory1</li>
+              <li className="catagory filter-item">Catagory2</li>
+              <li className="catagory filter-item">Catagory3</li>
+              <li className="catagory filter-item">Catagory4</li>
+            </ul>
+
+          </div>
+          <div className="gallery">
+            <img src="#"></img>
+          </div>
+
+        </div>
       </div>
     );
   }
