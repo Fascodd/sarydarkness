e6f07f64fc95033624500fdfe7515232f9e1fb99
Fascodd <45606128+Fascodd@users.noreply.github.com>
2020-08-02 23:04:09 -0700

    request page input animations and image positioning

--- sarydarkness/src/components/RequestPage.js
+++ sarydarkness/src/components/RequestPage.js
@@ -1,55 +1,69 @@
 import React from 'react'
 import '../styles/RequestPage.css';
+import gallery_img from '../pics/gallery_img'
 export default class RequestPage extends React.Component {
     constructor() {
         super();
     }
 
     componentDidMount() {
-        const artDisplayContainer = document.getElementById("art-display-container");
-
-
+        const requestPageCollage = gallery_img;
+        for (let i = 1; i <= 8; i++) {
+            const randomNum = Math.floor(Math.random() * (requestPageCollage.length ) );
+            const randomImgSrc = requestPageCollage[randomNum].src;
+            document.getElementById(`image${i}`).style.backgroundImage = `url(${randomImgSrc})`;
+            requestPageCollage.splice(randomNum, 1)
+        }
     }
     render() {
         return (
             < span >
                 <div className="page-container" id="request-page-container">
-                    <div id="art-display-cotnainer">
-                        <div className="art-display-image" id="image1"></div>
-                        <div className="art-display-image" id="image2"></div>
-                        <div className="art-display-image" id="image3"></div>
-                        <div className="art-display-image" id="image4"></div>
-                        <div className="art-display-image" id="image5"></div>
-                        <div className="art-display-image" id="image6"></div>
-                        <div className="art-display-image" id="image7"></div>
-                        <div className="art-display-image" id="image8"></div>
+                    <div id="art-display-container">
+                        <div id="art-display-container1">
+                            <div className="art-display-image" id="image1"></div>
+                            <div className="art-display-image" id="image2"></div>
+                            <div className="art-display-image" id="image3"></div>
+                            <div className="art-display-image" id="image4"></div></div>
+                        <div id="art-display-container2">
+                            <div className="art-display-image" id="image5"></div>
+                            <div className="art-display-image" id="image6"></div>
+                            <div className="art-display-image" id="image7"></div>
+                            <div className="art-display-image" id="image8"></div></div>
                     </div>
                     <div className="box-container" id="form-container" >
                         <form>
                             <div className="contact-wrapper">
                                 <div className=" name" id="first-name">
-                                    <label className="form-label"> First Name </label>
-                                    <input className="form-input" type="text" ></input>
+                                    <input className="form-input" type="text" autoComplete="off" required={true}></input>
+                                    <label className="form-label">
+                                        <span className="content-name">  First Name </span>
+                                    </label>
                                 </div>
                                 <div className=" name" id="last-name">
-                                    <label className="form-label"> Last Name </label>
-                                    <input className="form-input" type="text" ></input>
+                                    <input className="form-input" type="text" required={true} ></input>
+                                    <label className="form-label">
+                                        <span className="content-name"> Last Name</span>
+                                    </label>
                                 </div>
                             </div>
                             <div className="contact-wrapper" id="email-wrapper">
-                                <label className="form-label"> Email </label>
-                                <input className="form-input" id="email-input" type="text" >
-                                </input>
+                                <input className="form-input" id="email-input" type="text" required={true}></input>
+                                <label className="form-label">
+                                    <span className="content-name">Email </span>
+                                </label>
                             </div>
                             <div className="contact-wrapper" id="subject-wrapper">
-                                <label className="form-label"> Subject </label>
-                                <input className="form-input" id="subject-input" type="text" >
-                                </input>
+                                <input className="form-input" id="subject-input" type="text" required={true} ></input>
+                                <label className="form-label">
+                                    <span className="content-name"> Subject </span>
+                                </label>
                             </div>
                             <div className="contact-wrapper" id="message-wrapper">
-                                <label className="form-label"> Message </label>
+                                <label className="form-label">
+                                    <span className="content-name"> Message </span>
+                                </label>
                                 <textarea className="form-input" id="message-input" type="message"></textarea>
-
                             </div>
                             <div className="contact-wrapper" id="button-wrapper">
                                 <button> Submit </button>
